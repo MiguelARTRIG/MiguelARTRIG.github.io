0e91e0e4b2d3124629248b2b2c96c1ecf86b258b
MiguelARTRIG <miguel_artrig@hotmail.com>
2023-04-27 14:59:13 -0300

    Main Main

--- app/js/script.js
+++ app/js/script.js
@@ -1,26 +1,21 @@
+const menu_button = document.querySelector('.menu_button');
+const sidebar = document.querySelector('.sidebar');
+const xclick = document.querySelector('#xclick');
 
-  window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.main_header');
-    const itemlist = document.querySelectorAll('.item_list');
-    const logo = document.querySelector('#medite');
-    const tpontos = document.querySelector('#trespontos');
-    const button = document.querySelector('.button_1');
+
+
+menu_button.addEventListener('click', function(){
+    console.log('click me bitch');
+
+        sidebar.classList.add('open')
+    
+});
+
+xclick.addEventListener('click', function(){
+    console.log('clicked X');
+    
+        sidebar.classList.remove('open');
+    
   
-    if (window.scrollY > 0) {
-      navbar.classList.add('navbar-scroll');
-      logo.classList.add('medite_js');
-      button.classList.add('button_js');
-      tpontos.classList.add('trespontos_js')
-      itemlist.forEach(function(element) {
-        element.classList.add('item_listjs');
-      });
-    } else {
-      navbar.classList.remove('navbar-scroll');
-      logo.classList.remove('medite_js');
-      button.classList.remove('button_js');
-      tpontos.classList.remove('trespontos_js')
-      itemlist.forEach(function(element) {
-        element.classList.remove('item_listjs');
-      });
-    }
-  });
+
+})
