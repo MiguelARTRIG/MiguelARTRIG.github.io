3455f39b8eb994f439272618636aa3f20bb27bda
MiguelARTRIG <miguel_artrig@hotmail.com>
2023-05-25 15:15:49 -0300

    Nav
    nAV

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,19 @@
 const menu_button = document.querySelector('.menu_button');
 const sidebar = document.querySelector('.sidebar');
 const xclick = document.querySelector('#xclick');
+const hamburger = document.querySelector('.header_toggle');
+const MobileMen = document.querySelector('.mobile_menu');
 
-
+hamburger.addEventListener('click', function(){
+   if(hamburger.classList.contains('open')){
+        hamburger.classList.remove('open');
+        MobileMen.classList.remove('ShowMenu');
+    }
+ else{
+       hamburger.classList.add('open');
+       MobileMen.classList.add('ShowMenu');
+   }
+})
 
 menu_button.addEventListener('click', function(){
     console.log('click me bitch');
